dc7ec16a54a894b3be587aceb624962ffc6cfc93
Your Name <you@example.com>
2023-03-13 05:33:21 +0900

    검색 기본 세팅 구현

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["static.wikia.nocookie.net", "t1.daumcdn.net", "static.inven.co.kr"] // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+  }
 }
 
 module.exports = nextConfig
